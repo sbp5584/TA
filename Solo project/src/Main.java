d5a5396bb32a02e0672b8cd7f916543a7b57384e
sbp5584 <122904268+sbp5584@users.noreply.github.com>
2023-02-02 15:33:54 -0500

    Update Main.java

--- Solo project/src/Main.java
+++ Solo project/src/Main.java
@@ -8,19 +8,37 @@ public class Main {
         int dvalue = sword.getDamage();
         System.out.println(sword);
 
-        Weapon pillow = new Weapon("Soft Pillow","Pillow filled with feather", 1, 1);
-        pillow.setDamage(1);
-        int hit = pillow.getDamage();
-        System.out.println(pillow);
+        Pillow featherpillow = new Pillow();
+        System.out.println(featherpillow);
 
-        Weapon mace = new Weapon("Mace","Heavier then a sword", 15, 40);
-        mace.setDamage(35);
-        int power = mace.getDamage();
-        System.out.println(mace);
+        Mace spikedball = new Mace();
+        System.out.println(spikedball);
 
         Weapon arrow = new Weapon("Arrow","Shots from long range", 30, 50);
         arrow.setDamage(45);
         int speed = arrow.getDamage();
         System.out.println(arrow);
+
+        Enemy greenOgre = new Enemy("greenOgre", 30, 20 );
+        System.out.println(greenOgre);
+
+        Enemy redOgre = new Enemy("redOgre", 30, 20);
+        System.out.println(redOgre);
+
+        Enemy slowZombie = new Enemy("SlowZombie", 20, 15);
+        System.out.println(slowZombie);
+
+        Enemy fastZombie = new Enemy("FastZombie", 20, 15);
+        System.out.println(fastZombie);
+
+        Enemy spotDog = new Enemy("SpotDog", 10, 10);
+        System.out.println(spotDog);
+
+        Enemy GiantSpider = new Enemy("GiantSpider", 10, 5);
+        System.out.println(GiantSpider);
+
+        Enemy BlackBeard = new Enemy("DarkWizard",50,20);
+        System.out.println(BlackBeard);
     }
-}
+
+    }
