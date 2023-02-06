4210bdeb17286a94483dd843b376cd564f90496a
sbp5584 <122904268+sbp5584@users.noreply.github.com>
2023-02-06 13:19:52 -0500

    Update Dog.java

--- Solo project/src/Dog.java
+++ Solo project/src/Dog.java
@@ -1,5 +1,9 @@
 public class Dog extends Enemy{
-    public Dog(){
-        super("Dog", 10, 10);
+    private String desc;
+    private int smell_radius;
+    public Dog(String desc, int smell_radius){
+        super("SpotDog","Can smell blood" ,10, 10);
+        this.desc = desc;
+        this.smell_radius = smell_radius;
     }
 }
